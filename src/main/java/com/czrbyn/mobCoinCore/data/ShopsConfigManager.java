07a43a6d22407d3731d844d2605624f7b3239813
czrbynnn <corbynjearymain@gmail.com>
2025-05-16 09:56:42 +0100

    t

--- src/main/java/com/czrbyn/mobCoinCore/data/ShopsConfigManager.java
+++ src/main/java/com/czrbyn/mobCoinCore/data/ShopsConfigManager.java
@@ -3,7 +3,9 @@ package com.czrbyn.mobCoinCore.data;
 import com.czrbyn.mobCoinCore.MobCoinCore;
 import com.czrbyn.mobCoinCore.utils.ColorUtils;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,15 +35,56 @@ public class ShopsConfigManager {
     }
 
     public void createShop(String shopName, String[] args, CommandSender sender) {
-        if (exists(shopName)) {
-            sender.sendMessage(ColorUtils.colorize("&8[&bMobCoinsPro&8] &fShop already exists."));
-        } else {
+        if (!exists(shopName, sender) && hasDescription(shopName, args)) {
+            StringBuilder descriptionBuilder = new StringBuilder();
 
+            for (int i = 1; i < args.length; i++) {
+                descriptionBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    descriptionBuilder.append(" ");
+                }
+            }
+
+            String description = descriptionBuilder.toString();
+
+            createShopInFile(shopName, description, sender);
         }
     }
 
-    public boolean exists(String shopName) {
+    public boolean hasDescription(String shopName, String[] args) {
+        return args.length > 1;
+    }
 
+    public boolean createShopInFile(String shopName, String description, CommandSender sender) {
+        FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+        cfg.set("shops." + shopName + ".description", description);
+    }
+
+    public boolean exists(String shopName, CommandSender sender) {
+        if (!file.exists()) {
+            sender.sendMessage(ColorUtils.colorize("&8[&bMobCoinsPro&8] &fConfiguration File does not exist."));
+            return false;
+        } else {
+            FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+            if (config.get("shops." + shopName) != null) {
+                sender.sendMessage(ColorUtils.colorize("&8[&bMobCoinsPro&8] &fThis shop already exists."));
+                return false;
+            }
+
+            if (config.isConfigurationSection("shops")) {
+                ConfigurationSection shopsSection = config.getConfigurationSection("shops");
+
+                int shopCount = shopsSection.getKeys(false).size();
+
+                if (shopCount >= 4) {
+                    sender.sendMessage(ColorUtils.colorize("&8[&bMobCoinsPro&8] &fYou have reached the maximum number of shops (4)."));
+                    return false;
+                }
+
+            }
+            return true;
+        }
     }
 
 
